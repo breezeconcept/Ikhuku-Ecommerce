41010ea5f88e1d12aa24daa537aa4e5b5f896152
Breeze Concept <breezeconcept3@gmail.com>
2024-01-10 07:42:21 +0100

    updated 40

--- Products/models.py
+++ Products/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     # subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='products', null=True, default=get_default_subcategory)
-    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='products', null=True)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='products', blank=True, null=True)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     # Other fields...
 
